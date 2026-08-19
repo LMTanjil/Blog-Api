6ab6c18b245183def57a8b056140241ece63b91e
tanjil <tanjilalhussain@gmail.com>
2026-08-19 20:39:37 +0600

    fix: api responsive format fixed

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -9,9 +9,16 @@ export const createBlog = async (req, res) => {
             description,
             author
         });
-        return res.status(201).json(create_blog);
+        return res.status(201).json({
+            success: true,
+            message: "Blog created successfully",
+            data:create_blog
+        });
     }catch(err){
-        res.status(500).json(err);
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
     }
 }
 
@@ -19,19 +26,33 @@ export const getBlogbyId = async (req, res) => {
     const {id} = req.params;
     try{
        const singleBlog = await Blog.findById(id);
-       return res.status(200).json(singleBlog);
+       return res.status(200).json({
+           success: true,
+           message: "Blog found by id successfully",
+           data:singleBlog
+       });
     }catch(err){
-        res.status(500).json(err);
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
     }
 }
 
 export const getAllBlogs = async (req, res) => {
     try{
         const allBlogs = await Blog.find()
-        return res.status(200).json(allBlogs);
+        return res.status(200).json({
+            success: true,
+            message: "All blogs found successfully",
+            data:allBlogs
+        });
     }
     catch(err){
-        res.status(500).json(err);
+        res.status(500).json({
+            success: false,
+            message: err.message
+        });
     }
 }
 
